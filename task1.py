3ef8702589d83c8aa58d63c2675b32ab10f389b6
Maxim Fomin <fominm939@gmail.com>
2025-09-25 10:42:58 +0300

    ADD README

--- task1.py
+++ task1.py
@@ -1,11 +1,17 @@
 import sys
+def test_integer_interning():
+    interned_numbers = []
+    a = -10000
+    b = -9999
+    for i in range(-10000, 10000):
+        c = a + 1
+        if (id(b) == id(c)):
+            interned_numbers.append(b)
+        b += 1
+        a += 1
 
-def solution(a,b):
+    print("M = ", min(interned_numbers))
+    print("N = ", max(interned_numbers))
 
-    for i in range(a, b):
-        if id(i) != id(i):
-            return i
-    return -1
-
-print(solution(-1000000, 1000000))
+test_integer_interning()
 
